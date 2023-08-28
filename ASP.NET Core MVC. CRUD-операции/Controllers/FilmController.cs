1168a312cebdfa710d41d7be61e5f0507a4f1ab9
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-08-28 21:36:13 +0300

    #Task3 Создание класса FilmController и изменеие файла appsettings

---
+++ ASP.NET Core MVC. CRUD-операции/Controllers/FilmController.cs
@@ -0,0 +1,27 @@
+﻿using ASP.NET_Core_MVC._CRUD_операции.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace ASP.NET_Core_MVC._CRUD_операции.Controllers
+{
+	public class FilmController : Controller
+	{
+		private readonly FilmContext _context;
+
+		private readonly IWebHostEnvironment _appEnvironment;
+
+		public FilmController(FilmContext context, IWebHostEnvironment appEnvironment)
+		{
+			_context = context;
+			_appEnvironment = appEnvironment;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return _context.Films != null ?
+						View(await _context.Films.ToListAsync()) :
+						Problem("Entity set 'FilmContext.Films'  is null.");
+		}
+	}
+}
