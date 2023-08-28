5f54ba36b13a8c3b10ec9d7114430ab04cff3071
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-08-28 22:01:38 +0300

    #Task6 добавление методов GET  --  Create() и POST  -- Create(-----) в класс FilmController

--- ASP.NET Core MVC. CRUD-операции/Controllers/FilmController.cs
+++ ASP.NET Core MVC. CRUD-операции/Controllers/FilmController.cs
@@ -23,5 +23,54 @@ namespace ASP.NET_Core_MVC._CRUD_операции.Controllers
 						View(await _context.Films.ToListAsync()) :
 						Problem("Entity set 'FilmContext.Films'  is null.");
 		}
+
+		// GET: Films/Create
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		// POST: Films/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Film_Name,Film_director,Film_genre,Year_of_issue,poster,Short_description")] Film film, IFormFile uploadedFile)
+		{
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					if (uploadedFile != null)
+					{
+						string file_path = "/Image/" + uploadedFile.FileName;
+
+						using (var fileStream = new FileStream(_appEnvironment.WebRootPath + file_path, FileMode.Create))
+						{
+							await uploadedFile.CopyToAsync(fileStream); // копируем файл в поток
+						}
+						film.PosterPath = "~" + file_path;
+					}
+					_context.Add(film);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!FilmExists(film.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction("Index");
+			}
+			return View(film);
+		}
+
+		private bool FilmExists(int id)
+		{
+			return (_context.Films?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
 	}
 }
