9fb20293fd24fed5da955f07bb765477aa1e7664
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-08-28 21:31:38 +0300

    #Task2 Создание класса FilmContext, изменение файла Program, создание файла index(пустого) установка пакетов Nuget

--- ASP.NET Core MVC. CRUD-операции/Program.cs
+++ ASP.NET Core MVC. CRUD-операции/Program.cs
@@ -1,27 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using ASP.NET_Core_MVC._CRUD_операции.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Получаем строку подключения из файла конфигурации
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложениеs_MVC_
+builder.Services.AddDbContext<FilmContext>(options => options.UseSqlServer(connection));
+
+// Добавляем сервисы MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-	app.UseExceptionHandler("/Home/Error");
-	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-	app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
+app.UseStaticFiles(); // обрабатывает запросы к файлам в папке wwwroot s_MVCs_MVC_ControllerMVC
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Film}/{action=Index}/{id?}");
 
 app.Run();
